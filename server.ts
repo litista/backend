5ced380ef64c5d8eb6a89ee1c0dd61101f0e18eb
asim <asimshrestha608@gmail.com>
2025-01-19 20:19:58 +0545

    create express server & index page

--- server.ts
+++ server.ts
@@ -1,1 +1,5 @@
-console.log('Welcome to Litista Backend');
+import app from './src/app';
+import { PORT } from './src/config/env';
+
+const port = PORT;
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
