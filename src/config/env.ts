8eddc23551ae19c1623409216348129b9ef3dcd9
asim <asimshrestha608@gmail.com>
2025-01-25 13:00:58 +0545

    feat: login a user

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,3 +4,4 @@ export const PORT = process.env.PORT || 8000;
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const NODE_ENV = process.env.NODE_ENV || 'production';
 export const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
+export const JWT_SECRET = process.env.JWT_SECRET;
--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
+import bcrypt from 'bcrypt';
 
 import userModel from '../models/User';
+import { BAD_REQUEST, CREATED, OK, UNAUTHORIZED } from '../constants/http';
+import { signToken } from '../utils/jwt';
 
 export const registerHandler = async (
   req: Request,
@@ -24,7 +27,38 @@ export const registerHandler = async (
 
     const user = await userModel.create({ name, username, email, password });
 
-    res.status(201).json(user);
+    res.status(CREATED).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const loginHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { username, password } = req.body;
+
+    // validation
+    if (!username || !password) {
+      throw createHttpError(BAD_REQUEST, 'All fields are required');
+    }
+
+    const user = await userModel.findOne({ username }).select('+password');
+    if (!user) {
+      throw createHttpError(UNAUTHORIZED, 'Invalid username or password');
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      throw createHttpError(UNAUTHORIZED, 'Invalid username or password');
+    }
+
+    const accessToken = await signToken({ id: user._id });
+
+    res.status(OK).json({ message: 'login successfully', token: accessToken });
   } catch (error) {
     next(error);
   }
